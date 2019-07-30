bafc4571428cc1707606f7e7721d3675e9876355
TautvydasRudis <r_tautvydas@yahoo.com>
2019-07-30 18:29:41 +0300

    functions added

--- src/app/pages/Favorites/index.js
+++ src/app/pages/Favorites/index.js
@@ -2,30 +2,25 @@ import React from "react";
 import "./index.scss";
 import { ProductCard } from "../../components";
 
-function Favorites({
-  favorites,
-  products = [],
-  cart,
-  toggleFavorite,
-  addToCart,
-  removeFromCart
-}) {
+function Error() {
+  return <p>Ohh no, you don't have favorites</p>;
+}
+
+function Favorites({ favorites, products = [], cart, ...restProps }) {
   const favoriteProducts = products.filter(product =>
     favorites.includes(product.id)
   );
   return (
     <div className="Favorites">
-      {!favoriteProducts.length && <p>Ohh no, you don't have favorites</p>}
+      {!favoriteProducts.length && <Error />}
       {favoriteProducts.map(data => {
         const { count = 0 } = cart.find(({ id }) => id === data.id) || {};
 
         return (
           <ProductCard
+            {...restProps}
             {...data}
             key={data.id}
-            toggleFavorite={toggleFavorite}
-            removeFromCart={removeFromCart}
-            addToCart={addToCart}
             isFavorite
             cartCount={count}
           />
